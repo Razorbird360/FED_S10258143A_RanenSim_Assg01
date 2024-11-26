a5875ff5aebc049c5664dc1af83b2cec07984066
Ranen <ranensim.yangyi@gmail.com>
2024-11-26 20:33:32 +0800

    completed interactive search bar

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+const openIcon = document.querySelector('#search_icon');
+const closeIcon = document.querySelector('#cancel_icon');
+const container = document.querySelector('.search_container');
+const bar_bg = document.querySelector('.search_bar');
+
+openIcon.addEventListener('click', ()=>{
+    container.classList.remove('hide_container')
+    bar_bg.classList.remove('hide_bar_bg')
+    openIcon.src = "resources/Iconify/Search_black.svg"
+    closeIcon.src = "resources/Iconify/Cancel_black.svg"
+});
+closeIcon.addEventListener('click', ()=>{
+    container.classList.add('hide_container')
+    setTimeout(()=> {
+        bar_bg.classList.add('hide_bar_bg')
+        openIcon.src = "resources/Iconify/Search.svg"
+        closeIcon.src = "resources/Iconify/Cancel.svg"
+    },500)
+});
+
+
+
 let now = new Date();
 
 let UpcomingDay = new Date(
